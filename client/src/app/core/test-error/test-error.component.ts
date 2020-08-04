a2bc2cb1ca550ab3b51bef317f4e830050cf010a
pabitra89 <pabitra.manikpur@gmail.com>
2020-08-04 12:41:03 +0530

    End of the section 11

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'protractor';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  vaidationError: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe(response => {
+      console.log(response);
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
+    });
+  }
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(response => {
+      console.log(response);
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
+    });
+  }
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(response => {
+      console.log(response);
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
+    });
+  }
+  get404ValidationError() {
+    this.http.get(this.baseUrl + 'products/fourtytwo').subscribe(response => {
+      console.log(response);
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
+      this.vaidationError = error.errors;
+    });
+  }
+}
